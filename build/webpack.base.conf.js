51913f35f91d5f1f314ada1041ca1594e6487f77
Lan Zhang <zhany016@newschool.edu>
2020-02-21 13:45:48 -0500

    deploy test

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -19,8 +19,10 @@ module.exports = {
     path: config.build.assetsRoot,
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+      // ? config.build.assetsPublicPath
+      // : config.dev.assetsPublicPath
+      ? 'hack-comedy'
+      : '/'
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
